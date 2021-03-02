e6d6b071363a8c7a5e906e720f4c36d313c2abd0
Lavish Goyal <goellavish10@gmail.com>
2021-03-02 16:06:02 +0530

    User Validation Done

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+
 const app = express();
+
+// DB config
+const db = require('./config/keys').MongoURI;
+
+// Connect To Mongo
+mongoose.connect(db, { useNewUrlParser: true }, { useUnifiedTopology: true })
+ .then(()=> console.log('MongoDB Connected...'))
+ .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// Bodyparser
+app.use(express.urlencoded({extended: false}));
 
 // Routes
 app.use('/', require('./routes/index'));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://lavig_10:1234567890@cluster0.pgn6t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
