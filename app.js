499ebef2acf253181d3ec30acd3b0779a4ebc5b0
Lavish Goyal <goellavish10@gmail.com>
2021-03-02 19:22:17 +0530

    dashboard authenticated

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const mongoose = require('mongoose');
 const ejs = require('ejs');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+
+// Passport config
+require('./config/passport')(passport);
 
 // DB config
 const db = require('./config/keys').MongoURI;
@@ -29,6 +33,10 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect Flash
 app.use(flash());
 
@@ -36,6 +44,7 @@ app.use(flash());
 app.use((req,res,next)=>{
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
     next();
 });
 
