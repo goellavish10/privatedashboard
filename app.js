0a5c07ccc35e97d657b2d9b00f8d0dabc6c4391d
Lavish Goyal <goellavish10@gmail.com>
2021-03-03 17:09:12 +0530

    Navbar

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ require('./config/passport')(passport);
 const db = require('./config/keys').MongoURI;
 
 // Connect To Mongo
-mongoose.connect(db, { useNewUrlParser: true }, { useUnifiedTopology: true })
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
  .then(()=> console.log('MongoDB Connected...'))
  .catch(err => console.log(err));
 
